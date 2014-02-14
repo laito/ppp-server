f371e3d3e02d58e56eea51f18f4a845b39062947
Apoorv Singh <apoorv11028@iiitd.ac.in>
2014-02-15 00:05:21 +0530

    Rails new ppp-server

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PppServer::Application.config.secret_key_base = '4e0837d5ffa3acd97f487818d58cb6cb1e3a8f332772d9355d7184a29df5baf9e06aa91372ef96ffa96066b87b778a31c9b0d516d6568755504fb70d8a1d4c24'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PppServer::Application.config.session_store :cookie_store, key: '_ppp-server_session'
